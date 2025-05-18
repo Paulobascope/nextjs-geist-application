7fa3dafb870f9e249d8f88909baad9f6c467169c
Paulobascope <106777828+Paulobascope@users.noreply.github.com>
2025-05-18 14:01:20 -0300

    Add .next/static/chunks/src_app_personalize_page_tsx_a993527f._.js 1747587679819

---
+++ .next/static/chunks/src_app_personalize_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_b8416d50._.js",
+  "static/chunks/node_modules_64adcf53._.js"
+],
+    source: "dynamic"
+});
